fb13760df63709f16d812bcb3642de96e91107f1
Neha-rajbhar <nr470245@gmail.com>
2023-04-10 23:44:59 +0530

    second assignment

--- src/SecondQuestion/MouseOver.js
+++ src/SecondQuestion/MouseOver.js
@@ -10,7 +10,7 @@ function MouseOver() {
     
 }
 function onmouseout(event){
-    // Define function logic here
+   
     const name = event.target.getAttribute('dataname');
     setTimes([...times,{time: new Date().toLocaleString() ,eventmouse:'onMouseOut',id:name}]);
 
@@ -43,3 +43,28 @@ function onmouseout(event){
 }
 
 export default MouseOver;
+
+
+// {
+//   "1": [
+//     "create state ,store number , Generate table ",
+//     "cerate a button , update state with random value, update table"
+//   ],
+
+//   "2": [
+//     "create a h1",
+//     "create a h2",
+//     "create a div",
+//     "manage state to track that when user mouse overed on the h1 and when mouse out with date and time",
+//     "create a html table to show the logs.",
+//     [
+//       { "time": "", "eventName": "out or in" },
+//       { "time": "", "eventName": "out or in" }
+//     ]
+//   ],
+//   "3": [
+//     "create an array with multiple greetings",
+//     "create a button and handle click on button ",
+//     "show the next greeting message on h1"
+//   ]
+// }
