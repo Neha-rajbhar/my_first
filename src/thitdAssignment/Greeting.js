5311e623fa06dc8fdd0fc0e1d76590cfcc50902f
Neha-rajbhar <nr470245@gmail.com>
2023-04-10 23:41:24 +0530

    third assignment

--- src/thitdAssignment/Greeting.js
+++ src/thitdAssignment/Greeting.js
@@ -12,10 +12,34 @@ function Greeting() {
      
   return (
     <div>
-        <h1 style={{backgroundColor:'yellow'}} className={styles.greetings}>Greetings : {greet[currentNum]}</h1> {/* Use class name from module CSS */}
+        <h1 style={{backgroundColor:'yellow'}} className={styles.greetings}>Greetings : {greet[currentNum]}</h1>
             <button onClick={handleGreet}>Greet</button>
     </div>
   )
 }
 
 export default Greeting
+
+// {
+//   "1": [
+//     "create state ,store number , Generate table ",
+//     "cerate a button , update state with random value, update table"
+//   ],
+
+//   "2": [
+//     "create a h1",
+//     "create a h2",
+//     "create a div",
+//     "manage state to track that when user mouse overed on the h1 and when mouse out with date and time",
+//     "create a html table to show the logs.",
+//     [
+//       { "time": "", "eventName": "out or in" },
+//       { "time": "", "eventName": "out or in" }
+//     ]
+//   ],
+//   "3": [
+//     "create an array with multiple greetings",
+//     "create a button and handle click on button ",
+//     "show the next greeting message on h1"
+//   ]
+// }
