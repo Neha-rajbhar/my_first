da2165582cfa398912cf81a09b8d34d8dadbcebb
Neha-rajbhar <nr470245@gmail.com>
2023-04-11 21:38:03 +0530

    First assignment of revision day 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Greeting from './thitdAssignment/Greeting';
+
+import Fetch from './First/Fetch';
 
 function App() {
   return (
     <div className="App">
-     <Greeting/>
+     
+     <Fetch/>
     </div>
   );
 }
