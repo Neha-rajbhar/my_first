c1cd917643abf36cb9e002fbffaee7da7805bab5
Neha-rajbhar <nr470245@gmail.com>
2023-04-25 07:25:41 +0530

    redux-assignment

--- src/todo/TodoSlice.js
+++ src/todo/TodoSlice.js
@@ -1,34 +1,5 @@
 import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-// const initialState = {
-//   count: 0,
-//   todos: [],
-// };
-
-// export const todoSlice = createSlice({
-//   name: "todo",
-//   initialState,
-//   reducers: {
-//     addTodo: (state, action) => {
-//       const todo = {
-//         id: Math.random() * 100,
-//         text: action.payload,
-//       };
-//       state.todos.push(todo);
-//       state.count += 1;
-//     },
-//     removeTodo: (state, action) => {
-//       state.todos = state.todos.filter((todo) => todo.id !== action.payload);
-//       state.count -= 1;
-//     },
-//   },
-// });
-
-// export const { addTodo, removeTodo } = todoSlice.actions;
-
-
-
-
 export const todosSlice = createSlice({
   name: "todos",
   initialState: {
